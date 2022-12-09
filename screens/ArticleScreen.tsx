ab580950f565e75f945e8ba80735434495f04701
log901108 <log901108@gmail.com>
2022-12-09 22:04:29 +0900

    '15.5.3';

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,21 +1,39 @@
 import {RouteProp, useRoute} from '@react-navigation/core';
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
+import {useQuery} from 'react-query';
+import {getArticle} from '../api/articles';
+import {getComments} from '../api/comments';
 import {RootStackParamList} from './types';
 
 type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
 
 function ArticleScreen() {
   const {params} = useRoute<ArticleScreenRouteProp>();
+  const {id} = params;
+
+  const articleQuery = useQuery(['article', id], () => getArticle(id));
+  const commentsQuery = useQuery(['comments', id], () => getComments(id));
+
+  if (!articleQuery.data || !commentsQuery.data) {
+    return (
+      <ActivityIndicator size="large" style={styles.spinner} color="black" />
+    );
+  }
+
   return (
-    <View style={styles.block}>
-      <Text>{params.id}</Text>
+    <View>
+      <Text>{articleQuery.data.title}</Text>
+      <Text>{articleQuery.data.body}</Text>
+      <Text>{commentsQuery.data.length} of comments</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  spinner: {
+    flex: 1,
+  },
 });
 
 export default ArticleScreen;
