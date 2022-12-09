d6226b8890686d38db7611a2103402c8a74acb17
log901108 <log901108@gmail.com>
2022-12-09 22:31:36 +0900

    '15.5.4';

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,10 +1,19 @@
 import {RouteProp, useRoute} from '@react-navigation/core';
 import React from 'react';
-import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 import {useQuery} from 'react-query';
 import {getArticle} from '../api/articles';
 import {getComments} from '../api/comments';
 import {RootStackParamList} from './types';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import ArticleView from '../components/ArticleView';
+import CommentItem from '../components/CommentItem';
 
 type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
 
@@ -15,24 +24,52 @@ function ArticleScreen() {
   const articleQuery = useQuery(['article', id], () => getArticle(id));
   const commentsQuery = useQuery(['comments', id], () => getComments(id));
 
+  const {bottom} = useSafeAreaInsets();
+
   if (!articleQuery.data || !commentsQuery.data) {
     return (
       <ActivityIndicator size="large" style={styles.spinner} color="black" />
     );
   }
 
+  const {title, body, published_at, user} = articleQuery.data;
+
   return (
-    <View>
-      <Text>{articleQuery.data.title}</Text>
-      <Text>{articleQuery.data.body}</Text>
-      <Text>{commentsQuery.data.length} of comments</Text>
-    </View>
+    <FlatList
+      style={styles.flatList}
+      contentContainerStyle={[styles.flatListContent, {paddingBottom: bottom}]}
+      data={commentsQuery.data}
+      renderItem={({item}) => (
+        <CommentItem
+          id={item.id}
+          message={item.message}
+          publishedAt={item.published_at}
+          username={item.user.username}
+        />
+      )}
+      keyExtractor={item => item.id.toString()}
+      ListHeaderComponent={
+        <ArticleView
+          title={title}
+          body={body}
+          publishedAt={published_at}
+          username={user.username}
+        />
+      }
+    />
   );
 }
 
 const styles = StyleSheet.create({
   spinner: {
     flex: 1,
+  },
+  flatList: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  flatListContent: {
+    paddingHorizontal: 12,
   },
 });
 
